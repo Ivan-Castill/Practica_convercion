9488b1da442c82052080b310ee802875582e2bc5
Ivan-Castill <ivan2003castillo12345@gmail.comzx>
2024-11-07 09:54:33 -0500

    Practica

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+
+//int=>double=>string
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String []args){
+        int val1=25;
+        double con1;
+        String con2;
+
+        con1=(double)val1;
+        con2 = String.valueOf(val1);
+
+        System.out.println(" el valor int: "+val1+
+                "\n el valor double es : "+con1+
+                "\n el valor string es : "+con2);
+
+
     }
 }
